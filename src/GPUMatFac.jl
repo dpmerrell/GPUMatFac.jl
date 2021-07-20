5ab3054e0bc07b176da59380ac94f2ae0e33e8f0
David Merrell <davidpmerrell@gmail.com>
2021-07-20 04:02:01 +0000

    added src and *.toml

---
+++ src/GPUMatFac.jl
@@ -0,0 +1,23 @@
+
+module GPUMatFac
+
+
+include("model.jl")
+include("losses.jl")
+include("regularizer.jl")
+include("graph_util.jl")
+include("line_search.jl")
+
+if CUDA.has_cuda()
+    include("fit_cuda.jl")
+    include("transform_cuda.jl")
+else
+    include("fit.jl")
+    include("transform.jl")
+end
+
+
+BLAS.set_num_threads(1)
+
+
+end # module
